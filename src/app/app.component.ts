3c3ea48cce3cf04c35f8d25d2f71f019fa31506d
= <heakerson@gmail.com>
2021-12-08 18:14:40 -0600

    adding checks for formatting errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Expression } from 'src/models/math-object/factor/expression.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MathableNG';
+
+  ngOnInit(): void {
+    const mo = new Expression('((a+b)');
+    console.log(mo);
+  }
 }
--- src/models/math-object/math-object.model.ts
+++ src/models/math-object/math-object.model.ts
@@ -12,14 +12,29 @@ export abstract class MathObject {
         this.formattedInput = this.getFormattedInputString();
     }
 
+    protected checkFormattingErrors(): void {
+        if (!this.inputWhitespaceRemoved) {
+            throw new Error(`${this.constructor.name} Empty Input`);
+        }
+
+        const operatorError = StringFormatter.tooManyOperators(this.inputWhitespaceRemoved);
+
+        if (operatorError) {
+            throw new Error(`${this.constructor.name} Input: ${this.inputWhitespaceRemoved} => Malformed Operators: '${operatorError}'`);
+        }
+
+        const parenthesisError = StringFormatter.hasParenthesisCountMismatch(this.inputWhitespaceRemoved);
+
+        if (parenthesisError) {
+            throw new Error(`${this.constructor.name} Input: ${this.inputWhitespaceRemoved} => Parenthesis Count Mismatch`);
+        }
+    }
+
     protected getFormattedInputString(): string {
         return this.inputWhitespaceRemoved;
     }
 
-    protected checkFormattingErrors(): void {
-    }
-
-    toString(): string {
+    public toString(): string {
         return this.formattedInput;
     }
 }
