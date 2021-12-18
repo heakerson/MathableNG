82117c6987327d278e9dff4e319b4be45e4598e0
= <heakerson@gmail.com>
2021-12-18 12:59:20 -0600

    start of expression tests

--- src/tests/math-object/factor/expression.spec.ts
+++ src/tests/math-object/factor/expression.spec.ts
@@ -1,38 +1,75 @@
+import { Sign } from "src/models/math-object/enums.model";
 import { Expression } from "src/models/math-object/factor/expression.model";
+import { Term } from "src/models/math-object/term.model";
+import { StringFormatter } from "src/models/services/string-formatter.service";
+import { mathObjectConstructorTests } from "../math-object.spec";
+import { factorConstructorTests } from "./factor.spec";
 
-xdescribe('Expression', () => {
+export function expressionConstructorTests<TExpression extends Expression>(
+    additionalLabel: string,
+    tests: { input: string, children: string[], toString: string, sign: Sign }[],
+    builder: (input: string) => TExpression
+): void {
+
+    describe(`Expression Constructor Tests => ${additionalLabel}`, () => {
+        tests.forEach(test => {
+            it(`'${test.input}' => should populate base properties correctly`, () => {
+                const mo: TExpression = builder(test.input);
+
+                expect(mo.terms.map(c => c.toString())).toEqual(test.children);
+                expect(mo.termCount).toEqual(test.children.length);
+                expect(mo.isSingleTerm).toEqual(test.children.length === 1);
+            });
+        });
+    });
+}
+
+describe('Expression', () => {
+
+    describe('Constructor', () => {
+        const constructorTests: { input: string, children: string[], toString: string, sign: Sign }[] = [
+            { input: '(a-b)', children: ['a', '-b'], toString: '(a-b)', sign: Sign.Positive },
+            // { input: '-(a-b)', children: ['a', '-b'], toString: '(a-b)', sign: Sign.Negative },
+            // { input: '(a+b)', children: ['a', 'b'], toString: '(a+b)', sign: Sign.Positive },
+            // { input: ' a +b', toString: '(a+b)' },
+            // { input: '(a+b)', toString: '(a+b)' },
+            // { input: '-(a+b)', toString: '-(a+b)' },
+            // { input: '(a+  b) ^(x)', toString: '((a+b)^(x))' },
+            // { input: '-(a+b)^(x)', toString: '(-(a+b)^(x))' },
+            // { input: '-(a+b)^-(x)', toString: '(-(a+b)^-(x))' },
+            // { input: '(a+b)(x+y)', toString: '((a+b)*(x+y))' },
+            // { input: '(a+b)  -(x+y)', toString: '((a+b)-(x+y))' },
+            // { input: '-(a+b)(x+y)', toString: '(-(a+b)*(x+y))' },
+            // { input: '-(a+b)/(x+y)', toString: '(-(a+b)/(x+y))' },
+            // { input: 'a/b', toString: '(a/b)' },
+            // { input: '-a/b', toString: '(-a/b)' },
+            // { input: '-a^b', toString: '(-a^b)' },
+        ];
+
+        mathObjectConstructorTests('STANDARD Constructor', constructorTests, (input: string) => new Expression(input));
+        mathObjectConstructorTests('STATIC Constructor', constructorTests, (input: string) => {
+            const terms = StringFormatter.parseTermStrings(input).map(t => new Term(t));
+            return Expression.fromTerms(...terms);
+        });
+
+        factorConstructorTests('STANDARD Constructor', constructorTests, (input: string) => new Expression(input));
+        factorConstructorTests('STATIC Constructor', constructorTests, (input: string) => {
+            const terms = StringFormatter.parseTermStrings(input).map(t => new Term(t));
+            return Expression.fromTerms(...terms);
+        });
+
+        expressionConstructorTests('STANDARD Constructor', constructorTests, (input: string) => new Expression(input));
+        expressionConstructorTests('STATIC Constructor', constructorTests, (input: string) => {
+            const terms = StringFormatter.parseTermStrings(input).map(t => new Term(t));
+            return Expression.fromTerms(...terms);
+        });
+
+    });
 
     describe('Individual Methods', () => {
 
         describe('', () => {
 
-        });
-    });
-
-    describe('Constructor', () => {
-        const inputs: { input: string, toString: string }[] = [
-            { input: 'a+b', toString: '(a+b)' },
-            { input: ' a +b', toString: '(a+b)' },
-            { input: '(a+b)', toString: '(a+b)' },
-            { input: '-(a+b)', toString: '-(a+b)' },
-            { input: '(a+  b) ^(x)', toString: '((a+b)^(x))' },
-            { input: '-(a+b)^(x)', toString: '(-(a+b)^(x))' },
-            { input: '-(a+b)^-(x)', toString: '(-(a+b)^-(x))' },
-            { input: '(a+b)(x+y)', toString: '((a+b)*(x+y))' },
-            { input: '(a+b)  -(x+y)', toString: '((a+b)-(x+y))' },
-            { input: '-(a+b)(x+y)', toString: '(-(a+b)*(x+y))' },
-            { input: '-(a+b)/(x+y)', toString: '(-(a+b)/(x+y))' },
-            { input: 'a/b', toString: '(a/b)' },
-            { input: '-a/b', toString: '(-a/b)' },
-            { input: '-a^b', toString: '(-a^b)' },
-        ];
-
-        inputs.forEach((test) => {
-            const mo = new Expression(test.input);
-
-            it(`Should transform '${test.input}' to '${test.toString}'`, () => {
-                expect(mo.toString()).toEqual(test.toString);
-            });
         });
     });
 });
---
+++ src/tests/math-object/factor/factor.spec.ts
@@ -0,0 +1,19 @@
+import { Sign } from "src/models/math-object/enums.model";
+import { Factor } from "src/models/math-object/factor/factor.model";
+
+export function factorConstructorTests<TFactor extends Factor>(
+    additionalLabel: string,
+    tests: { input: string, children: string[], toString: string, sign: Sign }[],
+    builder: (input: string) => TFactor
+): void {
+
+    describe(`Factor Constructor Tests => ${additionalLabel}`, () => {
+        tests.forEach(test => {
+            it(`'${test.input}' => should populate base properties correctly`, () => {
+                const mo: TFactor = builder(test.input);
+
+                expect(mo.sign).toEqual(test.sign);
+            });
+        });
+    });
+}
