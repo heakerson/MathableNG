9f93343901690cc8ec0dc2dddcec9433a53d6611
= <heakerson@gmail.com>
2021-12-20 11:02:29 -0600

    integer is now a subclass of double

--- src/models/math-object/factor/number/integer.model.ts
+++ src/models/math-object/factor/number/integer.model.ts
@@ -1,17 +1,12 @@
-import { MathObject } from "src/models/math-object/math-object.model";
-import { RealNumber } from "./real-number.model";
+import { Double } from "./double.model";
 
-export class Integer extends RealNumber {
+export class Integer extends Double {
 
     get isEven() : boolean {
         return this.value % 2 === 0;
     }
 
     constructor(input: string) {
-        super(input, Number.parseFloat(input));
-    }
-
-    copy(): MathObject {
-        throw new Error("Method not implemented.");
+        super(input);
     }
 }
