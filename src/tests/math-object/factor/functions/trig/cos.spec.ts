50b17cd5d1885bb1d79e2a95b78c5f15a386f610
= <heakerson@gmail.com>
2021-12-22 09:24:40 -0600

    cos and csc tests

--- src/tests/math-object/factor/functions/trig/cos.spec.ts
+++ src/tests/math-object/factor/functions/trig/cos.spec.ts
@@ -1,27 +1,27 @@
 import { Sign } from "src/models/math-object/enums.model";
-import { Tan } from "src/models/math-object/factor/functions/trig/tan.model";
+import { Cos } from "src/models/math-object/factor/functions/trig/cos.model";
 import { Factory } from "src/models/services/factory.service";
 import { mathObjectConstructorTests } from "src/tests/math-object/math-object.spec";
 import { factorConstructorTests } from "../../factor.spec";
 import { functionConstructorTests } from "../function.spec";
 import { TrigConstrTest, trigConstructorTests } from "./trig.spec";
 
-describe('Tan', () => {
+describe('Cos', () => {
 
     describe('Constructor', () => {
         const constructorTests: TrigConstrTest[] = [
-            new TrigConstrTest({ input: 'a', children: ['a'], fnString: 'tan', toString: 'tan[a]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: '-a', children: ['-a'], fnString: 'tan', toString: 'tan[-a]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: '-a+ c ', children: ['(-a+c)'], fnString: 'tan', toString: '-tan[(-a+c)]', sign: Sign.Negative }),
-            new TrigConstrTest({ input: '(-a+ c) ', children: ['(-a+c)'], fnString: 'tan', toString: '-tan[(-a+c)]', sign: Sign.Negative }),
-            new TrigConstrTest({ input: 'a/b', children: ['(a/b)'], fnString: 'tan', toString: 'tan[(a/b)]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: 'sin[x+t/-v]', children: ['sin[(x+(t/-v))]'], fnString: 'tan', toString: '-tan[sin[(x+(t/-v))]]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: 'a', children: ['a'], fnString: 'cos', toString: 'cos[a]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: '-a', children: ['-a'], fnString: 'cos', toString: 'cos[-a]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: '-a+ c ', children: ['(-a+c)'], fnString: 'cos', toString: '-cos[(-a+c)]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: '(-a+ c) ', children: ['(-a+c)'], fnString: 'cos', toString: '-cos[(-a+c)]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: 'a/b', children: ['(a/b)'], fnString: 'cos', toString: 'cos[(a/b)]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: 'tan[x+t/-v]', children: ['tan[(x+(t/-v))]'], fnString: 'cos', toString: '-cos[tan[(x+(t/-v))]]', sign: Sign.Negative }),
         ];
 
-        const standardBuilder = (test: TrigConstrTest) => new Tan(test.input, test.sign);
+        const standardBuilder = (test: TrigConstrTest) => new Cos(test.input, test.sign);
         const staticBuilder = (test: TrigConstrTest) => {
             const contents = Factory.buildFactor(test.input);
-            return Tan.fromFactor(contents, test.sign);
+            return Cos.fromFactor(contents, test.sign);
         };
 
         mathObjectConstructorTests('STANDARD Constructor', constructorTests, standardBuilder);
--- src/tests/math-object/factor/functions/trig/csc.spec.ts
+++ src/tests/math-object/factor/functions/trig/csc.spec.ts
@@ -1,27 +1,27 @@
 import { Sign } from "src/models/math-object/enums.model";
-import { Tan } from "src/models/math-object/factor/functions/trig/tan.model";
+import { Csc } from "src/models/math-object/factor/functions/trig/csc.model";
 import { Factory } from "src/models/services/factory.service";
 import { mathObjectConstructorTests } from "src/tests/math-object/math-object.spec";
 import { factorConstructorTests } from "../../factor.spec";
 import { functionConstructorTests } from "../function.spec";
 import { TrigConstrTest, trigConstructorTests } from "./trig.spec";
 
-describe('Tan', () => {
+describe('Csc', () => {
 
     describe('Constructor', () => {
         const constructorTests: TrigConstrTest[] = [
-            new TrigConstrTest({ input: 'a', children: ['a'], fnString: 'tan', toString: 'tan[a]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: '-a', children: ['-a'], fnString: 'tan', toString: 'tan[-a]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: '-a+ c ', children: ['(-a+c)'], fnString: 'tan', toString: '-tan[(-a+c)]', sign: Sign.Negative }),
-            new TrigConstrTest({ input: '(-a+ c) ', children: ['(-a+c)'], fnString: 'tan', toString: '-tan[(-a+c)]', sign: Sign.Negative }),
-            new TrigConstrTest({ input: 'a/b', children: ['(a/b)'], fnString: 'tan', toString: 'tan[(a/b)]', sign: Sign.Positive }),
-            new TrigConstrTest({ input: 'sin[x+t/-v]', children: ['sin[(x+(t/-v))]'], fnString: 'tan', toString: '-tan[sin[(x+(t/-v))]]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: 'a', children: ['a'], fnString: 'csc', toString: 'csc[a]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: '-a', children: ['-a'], fnString: 'csc', toString: 'csc[-a]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: '-a+ c ', children: ['(-a+c)'], fnString: 'csc', toString: '-csc[(-a+c)]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: '(-a+ c) ', children: ['(-a+c)'], fnString: 'csc', toString: '-csc[(-a+c)]', sign: Sign.Negative }),
+            new TrigConstrTest({ input: 'a/b', children: ['(a/b)'], fnString: 'csc', toString: 'csc[(a/b)]', sign: Sign.Positive }),
+            new TrigConstrTest({ input: 'tan[x+t/-v]', children: ['tan[(x+(t/-v))]'], fnString: 'csc', toString: '-csc[tan[(x+(t/-v))]]', sign: Sign.Negative }),
         ];
 
-        const standardBuilder = (test: TrigConstrTest) => new Tan(test.input, test.sign);
+        const standardBuilder = (test: TrigConstrTest) => new Csc(test.input, test.sign);
         const staticBuilder = (test: TrigConstrTest) => {
             const contents = Factory.buildFactor(test.input);
-            return Tan.fromFactor(contents, test.sign);
+            return Csc.fromFactor(contents, test.sign);
         };
 
         mathObjectConstructorTests('STANDARD Constructor', constructorTests, standardBuilder);
