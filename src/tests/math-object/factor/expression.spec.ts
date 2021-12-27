9593932ce694e6589a85fd5adee0e774c9c26065
= <heakerson@gmail.com>
2021-12-27 15:12:43 -0600

    expression static tests

--- src/tests/math-object/factor/expression.spec.ts
+++ src/tests/math-object/factor/expression.spec.ts
@@ -1,11 +1,21 @@
 import { Operators, Sign } from "src/models/math-object/enums.model";
 import { Expression } from "src/models/math-object/factor/expression.model";
 import { Term } from "src/models/math-object/term.model";
+import { ErrorCodes } from "src/models/services/error-handler.service";
 import { StringFormatter } from "src/models/services/string-formatter.service";
 import { baseMathObjectErrorTests, mathObjectConstructorErrorTests, mathObjectConstructorTests } from "../math-object.spec";
 import { factorConstructorTests, FactorConstTest } from "./factor.spec";
 
-export function expressionConstructorTests<TExpression extends Expression, TTest extends FactorConstTest>(
+export class ExpressionConstTest extends FactorConstTest {
+    additionalOperators?: { termIndex: number, addtionalOperator: Operators }[]
+
+    constructor(props: Partial<ExpressionConstTest>) {
+        super(props);
+        Object.assign(this, props);
+    }
+}
+
+export function expressionConstructorTests<TExpression extends Expression, TTest extends ExpressionConstTest>(
     additionalLabel: string,
     tests: TTest[],
     builder: (test: TTest) => TExpression
@@ -28,8 +38,8 @@ export function expressionConstructorTests<TExpression extends Expression, TTest
 describe('Expression', () => {
 
     describe('Constructor Tests', () => {
-        const standardBuilder = (test: FactorConstTest) => new Expression(test.input);
-        const staticBuilder = (test: FactorConstTest) => {
+        const standardBuilder = (test: ExpressionConstTest) => new Expression(test.input);
+        const staticBuilder = (test: ExpressionConstTest) => {
             let termStrings = StringFormatter.parseTermStrings(test.input);
 
             const additionalOps: { termIndex: number, addtionalOperator: Operators }[] = [];
@@ -55,30 +65,30 @@ describe('Expression', () => {
         };
 
         describe('Success', () => {
-            const constructorTests: FactorConstTest[] = [
-                new FactorConstTest({ input: '(a-b)', children: ['a', '-b'], toString: '(a-b)', sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a-b)', children: ['a', '-b'], toString: '-(a-b)', sign: Sign.Negative }),
-                new FactorConstTest({ input: '-(a+b)', children: ['a', 'b'], toString: '-(a+b)', sign: Sign.Negative }),
-                new FactorConstTest({ input: '(a+b)', children: ['a', 'b'], toString: '(a+b)', sign: Sign.Positive }),
-                new FactorConstTest({ input: ' a +b', children: ['a', 'b'], toString: '(a+b)', sign: Sign.Positive }),
-                new FactorConstTest({ input: ' -a +b', children: ['-a', 'b'], toString: '(-a+b)', sign: Sign.Positive }),
-                new FactorConstTest({ input: '(a+  b) ^(x)', children: ['(a+b)^(x)'], toString: '((a+b)^(x))', sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+  b) ^(x)', children: ['-(a+b)^(x)'], toString: '(-(a+b)^(x))', sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+b)^-(x)', children: ['-(a+b)^-(x)'], toString: '(-(a+b)^-(x))', sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+-b)', children: ['a', '-b'], toString: '-(a+-b)', sign: Sign.Negative }),
-                new FactorConstTest({ input: '-(a--b)', children: ['a', '-b'], toString: '-(a--b)', sign: Sign.Negative }),
-                new FactorConstTest({ input: 'a--b', children: ['a', '-b'], toString: '(a--b)', sign: Sign.Positive }),
-                new FactorConstTest({ input: '(a+b)(x+y)', children: ['(a+b)*(x+y)'], toString: '((a+b)*(x+y))', sign: Sign.Positive }),
-                new FactorConstTest({ input: '(a+b)(x+y)-sin[a^b]', children: ['(a+b)*(x+y)', '-sin[a^b]'], toString: '((a+b)*(x+y)-sin[a^b])', sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+b)^(x)', toString: '(-(a+b)^(x))', children: ['-(a+b)^(x)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+b)^-(x)', toString: '(-(a+b)^-(x))', children: ['-(a+b)^-(x)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '(a+b)  -(x+y)', toString: '((a+b)-(x+y))', children: ['(a+b)', '-(x+y)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(a+b)(x+y)', toString: '(-(a+b)*(x+y))', children: ['-(a+b)*(x+y)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '-(-(a+b)(x+y))', toString: '-(-(a+b)*(x+y))', children: ['-(a+b)*(x+y)'], sign: Sign.Negative }),
-                new FactorConstTest({ input: '-(a+b)/(x+y)', toString: '((-(a+b)/(x+y)))', children: ['(-(a+b)/(x+y))'], sign: Sign.Positive }),
-                new FactorConstTest({ input: 'a/b', toString: '((a/b))', children: ['(a/b)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '-a/b', toString: '((-a/b))', children: ['(-a/b)'], sign: Sign.Positive }),
-                new FactorConstTest({ input: '-a^b', toString: '(-a^b)', children: ['-a^b'], sign: Sign.Positive }),
+            const constructorTests: ExpressionConstTest[] = [
+                new ExpressionConstTest({ input: '(a-b)', children: ['a', '-b'], toString: '(a-b)', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a-b)', children: ['a', '-b'], toString: '-(a-b)', sign: Sign.Negative }),
+                new ExpressionConstTest({ input: '-(a+b)', children: ['a', 'b'], toString: '-(a+b)', sign: Sign.Negative }),
+                new ExpressionConstTest({ input: '(a+b)', children: ['a', 'b'], toString: '(a+b)', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: ' a +b', children: ['a', 'b'], toString: '(a+b)', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: ' -a +b', children: ['-a', 'b'], toString: '(-a+b)', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '(a+  b) ^(x)', children: ['(a+b)^(x)'], toString: '((a+b)^(x))', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+  b) ^(x)', children: ['-(a+b)^(x)'], toString: '(-(a+b)^(x))', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+b)^-(x)', children: ['-(a+b)^-(x)'], toString: '(-(a+b)^-(x))', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+-b)', children: ['a', '-b'], toString: '-(a+-b)', sign: Sign.Negative }),
+                new ExpressionConstTest({ input: '-(a--b)', children: ['a', '-b'], toString: '-(a--b)', sign: Sign.Negative }),
+                new ExpressionConstTest({ input: 'a--b', children: ['a', '-b'], toString: '(a--b)', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '(a+b)(x+y)', children: ['(a+b)*(x+y)'], toString: '((a+b)*(x+y))', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '(a+b)(x+y)-sin[a^b]', children: ['(a+b)*(x+y)', '-sin[a^b]'], toString: '((a+b)*(x+y)-sin[a^b])', sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+b)^(x)', toString: '(-(a+b)^(x))', children: ['-(a+b)^(x)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+b)^-(x)', toString: '(-(a+b)^-(x))', children: ['-(a+b)^-(x)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '(a+b)  -(x+y)', toString: '((a+b)-(x+y))', children: ['(a+b)', '-(x+y)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(a+b)(x+y)', toString: '(-(a+b)*(x+y))', children: ['-(a+b)*(x+y)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-(-(a+b)(x+y))', toString: '-(-(a+b)*(x+y))', children: ['-(a+b)*(x+y)'], sign: Sign.Negative }),
+                new ExpressionConstTest({ input: '-(a+b)/(x+y)', toString: '((-(a+b)/(x+y)))', children: ['(-(a+b)/(x+y))'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: 'a/b', toString: '((a/b))', children: ['(a/b)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-a/b', toString: '((-a/b))', children: ['(-a/b)'], sign: Sign.Positive }),
+                new ExpressionConstTest({ input: '-a^b', toString: '(-a^b)', children: ['-a^b'], sign: Sign.Positive }),
             ];
     
             mathObjectConstructorTests('STANDARD Constructor', constructorTests, standardBuilder);
@@ -92,11 +102,31 @@ describe('Expression', () => {
         });
 
         describe('Errors', () => {
-            const constructorTests: FactorConstTest[] = baseMathObjectErrorTests.map(ex => {
-                return new FactorConstTest({ input: ex.input, errorCode: ex.errorCode, children: [], toString: '' });
-            })
+            const staticErrorTestInputs: { children: string[], errorCode: ErrorCodes, additionalOperators?: { termIndex: number, addtionalOperator: Operators }[] }[] = [
+                { children: [''], errorCode: ErrorCodes.EMPTY },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: -1, addtionalOperator: Operators.Subtraction }], errorCode: ErrorCodes.Expression.INVALID_ADDITIONAL_OPERATOR_INDEX },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: -5, addtionalOperator: Operators.Subtraction }], errorCode: ErrorCodes.Expression.INVALID_ADDITIONAL_OPERATOR_INDEX },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: 1, addtionalOperator: Operators.Multiplication }], errorCode: ErrorCodes.Expression.INVALID_ADDITIONAL_OPERATOR },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: 1, addtionalOperator: Operators.Division }], errorCode: ErrorCodes.Expression.INVALID_ADDITIONAL_OPERATOR },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: 1, addtionalOperator: Operators.Addition }], errorCode: ErrorCodes.MALFORMED_OPERATORS },
+                { children: ['a', 'b'], additionalOperators: [{ termIndex: 1, addtionalOperator: Operators.Subtraction }], errorCode: ErrorCodes.MALFORMED_OPERATORS },
+            ];
+
+            const staticErrorTests = staticErrorTestInputs.map(ex => {
+                return new ExpressionConstTest({ input: '', errorCode: ex.errorCode as number, children: ex.children, toString: '', additionalOperators: ex.additionalOperators });
+            });
+
+            const staticErrorBuilder = (test: ExpressionConstTest) => {
+                const terms = test.children.map(t => new Term(t));
+                return Expression.fromTerms(terms, test.additionalOperators);
+            }
+
+            const constructorTests: ExpressionConstTest[] = baseMathObjectErrorTests.map(ex => {
+                return new ExpressionConstTest({ input: ex.input, errorCode: ex.errorCode, children: [], toString: '' });
+            });
     
             mathObjectConstructorErrorTests('STANDARD Constructor', constructorTests, standardBuilder);
+            mathObjectConstructorErrorTests('STATIC Constructor', staticErrorTests, staticErrorBuilder);
         });
     });
 
